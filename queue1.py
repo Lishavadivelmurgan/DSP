284bd34fadba2df6c69b1286d395e92559dc6c4d
Lishavadivelmurgan <LISHAMURGAN2106@GMAIL.COM>
2025-08-13 09:57:14 +0530

    Add files via upload

---
+++ queue1.py
@@ -0,0 +1,19 @@
+class queue:
+    def __init__(self):
+        self.queue=[]
+
+    def enqueue(self,item):
+        self.queue.append(item)
+        print(f"pushed{item}")
+
+    def dequeue(self):
+        return(f"poped:{self.queue.pop()}")
+    
+    def size(self):
+        return len(self.queue)
+q = queue()
+q.enqueue("ana")
+q.enqueue("bob")
+print(f"queue size :{q.size()}")
+
+
